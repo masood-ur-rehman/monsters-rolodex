404d00077de6b8ace7c0476daff7a9211534daa0
Masood <masoodrehman39@gmail.com>
2020-11-18 23:34:13 +0500

    Updates

---
+++ precache-manifest.c09621bf9cf3f19d9e6bef3474648889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095d6b2d156f441c76b74eb6534fdecc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5b97d8294bc8e37908b7",
+    "url": "/monsters-rolodex/static/css/main.3758d7a4.chunk.css"
+  },
+  {
+    "revision": "0b31f040e3b094246ab9",
+    "url": "/monsters-rolodex/static/js/2.f8df9ad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.f8df9ad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b97d8294bc8e37908b7",
+    "url": "/monsters-rolodex/static/js/main.11719346.chunk.js"
+  },
+  {
+    "revision": "f7dad11301a511665114",
+    "url": "/monsters-rolodex/static/js/runtime-main.722e3c0c.js"
+  }
+]);
